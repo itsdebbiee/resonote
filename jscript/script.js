9297a30ef3f98292f2bf4e4d862d8932a3ae12a7
Debbie Pan <xdebbiee@gmail.com>
2014-02-13 23:39:01 -0800

    UGH can't get json/ ajax to work.

--- jscript/script.js
+++ jscript/script.js
@@ -1,2 +1,22 @@
 /* Add new element into list */ 
 
+var data;
+$('#b1').click(function() {
+	$('#wait').show();
+	$.get("/data", callbackFn);
+	console.log('AJAX');
+})
+
+function callbackFn(result) {
+	$('#wait').hide();
+	data= result['wordbank'] + "!";
+	$('.container').append(data);
+	console.log('its working')
+}
+
+/*adding html
+function addWord(result) {
+	console.log(result);
+	var projectHTML = ''
+}
+*/
