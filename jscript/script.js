28ccf77f021b34170ca9796544799ee138cc5a7d
Debbie Pan <xdebbiee@gmail.com>
2014-02-20 16:05:44 -0800

    added all the songs + jquery for those pages

--- jscript/script.js
+++ jscript/script.js
@@ -15,3 +15,71 @@ function f(e) {
 }
 
 
+$(document).ready(function() {
+	$("#ouest").click(function() {
+		$("#ouest-reveal").fadeIn('fast');
+		$("#ouest, #sympathique").fadeOut('fast');
+		$(".back").fadeIn('fast');
+	});
+
+	$("#sympathique").click(function() {
+		$("#sympathique-reveal").fadeIn('fast');
+		$("#ouest, #sympathique").fadeOut('fast');
+		$(".back").fadeIn('fast');
+	});
+
+	$("#quand").click(function() {
+		$("#quand-reveal").fadeIn('fast');
+		$("#quand, #quel").fadeOut('fast');
+		$(".back1").fadeIn('fast');
+	});
+
+	$("#quel").click(function() {
+		$("#quel-reveal").fadeIn('fast');
+		$("#quel, #quand").fadeOut('fast');
+		$(".back1").fadeIn('fast');
+	});
+
+	$("#tous").click(function() {
+		$("#tous-reveal").fadeIn('fast');
+		$("#tous, #papa").fadeOut('fast');
+		$(".back2").fadeIn('fast');
+	});
+
+	$("#papa").click(function() {
+		$("#papa-reveal").fadeIn('fast');
+		$("#tous, #papa").fadeOut('fast');
+		$(".back2").fadeIn('fast');
+	});
+
+	$(".back").click(function() {
+		$("#ouest, #sympathique").fadeIn('fast');
+		$(".back, #sympathique-reveal, #ouest-reveal").fadeOut('fast');
+	});
+
+	$(".back1").click(function() {
+		$("#quel, #quand").fadeIn('fast');
+		$(".back1, #quel-reveal, #quand-reveal").fadeOut('fast');
+	});
+
+	$(".back2").click(function() {
+		$("#tous, #papa").fadeIn('fast');
+		$(".back2, #tous-reveal, #papa-reveal").fadeOut('fast');
+	});
+
+
+});
+
+/* Popover */
+
+$(document).ready(function() {
+	$(".line a").popover({html:true});
+
+	$(".line a").click(function(e) {
+		e.preventDefault();
+	});
+});
+
+
+
+
